7ace3ec8255de92a1fbe6784fae66b6d89ee27c9
yfktn <friskantoni@gmail.com>
2022-07-28 11:20:05 +0700

    Tambahkan untuk hasil survey checkbox

--- components/GrafisHasilSurveyComponent.php
+++ components/GrafisHasilSurveyComponent.php
@@ -39,19 +39,33 @@ class GrafisHasilSurveyComponent extends ComponentBase
             'defer' => true
         ]);
 
-        $sqlSelector = <<<SQL
-select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, jawaban.jawaban, count(*) as jumlah
+        $sqlSelectorRadio = <<<SQL
+select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, jawaban.jawaban as jawabannya_user, count(*) as jumlah
 from yfktn_surveykepuasan_ survey
 inner join yfktn_surveykepuasan_pertanyaan pertanyaan on survey.id = pertanyaan.survey_id 
     and pertanyaan.cara_menjawab in ('radio')
 inner join yfktn_surveykepuasan_jawaban jawaban on survey.id = jawaban.survey_id 
     and pertanyaan.id = jawaban.pertanyaan_id
 where survey.slug = ?
-group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawaban.jawaban
-
+group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user
+SQL;
+        $sqlSelectorCheckBox = <<<SQL
+select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, JT.pilihan as jawabannya_user, count(*) as jumlah
+from yfktn_surveykepuasan_jawaban as jawaban, 
+    yfktn_surveykepuasan_ as survey, 
+    yfktn_surveykepuasan_pertanyaan as pertanyaan,
+JSON_TABLE(
+    jawaban.jawaban,
+    '$[*]' columns(pilihan varchar(30) path '$')
+) as JT 
+where survey.id = jawaban.survey_id and survey.slug = ? and pertanyaan.survey_id = survey.id
+group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user
 SQL;
 
-        $hasilJawaban = DB::select($sqlSelector, [$this->property('slug')]);
+        $hasilJawaban = collect(DB::select($sqlSelectorRadio, [$this->property('slug')]));
+        $hasilJawaban = $hasilJawaban->merge(
+            DB::select($sqlSelectorCheckBox, [$this->property('slug')])
+        );
         $untukGrafis = [];
         $currentPertanyaan = '';
         // mari kita generate struktur datanya!
@@ -62,10 +76,15 @@ SQL;
                 $untukGrafis[$currentPertanyaan]['data'] = [];
                 $untukGrafis[$currentPertanyaan]['label'] = $hasil->pertanyaan;
             }
-            $untukGrafis[$currentPertanyaan]['labels'][] = $hasil->jawaban . ' ' . $hasil->jumlah;
+            $untukGrafis[$currentPertanyaan]['labels'][] = $hasil->jawabannya_user /*. ' ' . $hasil->jumlah*/;
             $untukGrafis[$currentPertanyaan]['data'][] = $hasil->jumlah;
         }
         $this->hasilSurveyTerpilih = $untukGrafis;
 
+    }
+
+    public function renderStrukturDataUntukTipeDataRadio()
+    {
+        
     }
 }
