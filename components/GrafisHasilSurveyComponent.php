6dc075b6d61fc2bb83f009d2c529c83d6972fc07
yfktn <friskantoni@gmail.com>
2022-08-22 21:08:42 +0700

    Tambahkan fungsi untuk warna output grafis

--- components/GrafisHasilSurveyComponent.php
+++ components/GrafisHasilSurveyComponent.php
@@ -40,7 +40,8 @@ class GrafisHasilSurveyComponent extends ComponentBase
         ]);
 
         $sqlSelectorRadio = <<<SQL
-select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, jawaban.jawaban as jawabannya_user, count(*) as jumlah
+select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, 
+    jawaban.jawaban as jawabannya_user, count(*) as jumlah, pertanyaan.pilihan as pertanyaan_pilihan
 from yfktn_surveykepuasan_ survey
 inner join yfktn_surveykepuasan_pertanyaan pertanyaan on survey.id = pertanyaan.survey_id 
     and pertanyaan.cara_menjawab in ('radio')
@@ -50,7 +51,8 @@ where survey.slug = ?
 group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user
 SQL;
         $sqlSelectorCheckBox = <<<SQL
-select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, JT.pilihan as jawabannya_user, count(*) as jumlah
+select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, 
+    JT.pilihan as jawabannya_user, count(*) as jumlah, pertanyaan.pilihan as pertanyaan_pilihan
 from yfktn_surveykepuasan_jawaban as jawaban, 
     yfktn_surveykepuasan_ as survey, 
     yfktn_surveykepuasan_pertanyaan as pertanyaan,
@@ -68,17 +70,40 @@ SQL;
             DB::select($sqlSelectorCheckBox, [$this->property('slug')])
         );
         $untukGrafis = [];
+        $indexWarna = [];
+        $indexWarnaDefault = [
+            "#FF6384",
+            "#63FF84",
+            "#84FF63",
+            "#8463FF",
+            "#6384FF"];
+        $indexWarnaDefaultOffset = 0;
         $currentPertanyaan = '';
         // mari kita generate struktur datanya!
         foreach($hasilJawaban as $hasil) {
             if($currentPertanyaan != $hasil->pertanyaannya_id) {
                 $currentPertanyaan = $hasil->pertanyaannya_id;
+                // dapatkan untuk grafis?
+                $indexWarna = [];
+                $pilihan = json_decode($hasil->pertanyaan_pilihan);
+                foreach($pilihan as $p) {
+                    $indexWarna[$p->pilihan_label] = $p->pilihan_warna;
+                }
                 $untukGrafis[$currentPertanyaan]['labels'] = [];
                 $untukGrafis[$currentPertanyaan]['data'] = [];
+                $untukGrafis[$currentPertanyaan]['warna'] = [];
                 $untukGrafis[$currentPertanyaan]['label'] = $hasil->pertanyaan;
             }
             $untukGrafis[$currentPertanyaan]['labels'][] = $hasil->jawabannya_user /*. ' ' . $hasil->jumlah*/;
             $untukGrafis[$currentPertanyaan]['data'][] = $hasil->jumlah;
+            if(isset($indexWarna[$hasil->jawabannya_user])) {
+                $untukGrafis[$currentPertanyaan]['warna'][] = $indexWarna[$hasil->jawabannya_user]; 
+            } else {
+                if($indexWarnaDefaultOffset + 1 > 4) {
+                    $indexWarnaDefaultOffset = 0;
+                }
+                $untukGrafis[$currentPertanyaan]['warna'][] = $indexWarnaDefault[$indexWarnaDefaultOffset]; 
+            }
         }
         $this->hasilSurveyTerpilih = $untukGrafis;
 
