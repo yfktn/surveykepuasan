c436da2a6b5f5caf5d44e580ab3e408f03fb54a0
yfktn <friskantoni@gmail.com>
2022-10-06 14:06:00 +0700

    Update chartjs dan gub checkbox graphic

--- components/GrafisHasilSurveyComponent.php
+++ components/GrafisHasilSurveyComponent.php
@@ -42,32 +42,29 @@ class GrafisHasilSurveyComponent extends ComponentBase
         $sqlSelectorRadio = <<<SQL
 select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, 
     jawaban.jawaban as jawabannya_user, count(*) as jumlah, pertanyaan.pilihan as pertanyaan_pilihan
-from yfktn_surveykepuasan_ survey
-inner join yfktn_surveykepuasan_pertanyaan pertanyaan on survey.id = pertanyaan.survey_id 
+from yfktn_surveykepuasan_ as survey
+inner join yfktn_surveykepuasan_pertanyaan as pertanyaan on survey.id = pertanyaan.survey_id 
     and pertanyaan.cara_menjawab in ('radio')
-inner join yfktn_surveykepuasan_jawaban jawaban on survey.id = jawaban.survey_id 
+inner join yfktn_surveykepuasan_jawaban as jawaban on survey.id = jawaban.survey_id 
     and pertanyaan.id = jawaban.pertanyaan_id
 where survey.slug = ?
-group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user
+group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user, pertanyaan_pilihan
 SQL;
         $sqlSelectorCheckBox = <<<SQL
 select survey.nama, pertanyaan.id as pertanyaannya_id, pertanyaan.pertanyaan, 
-    JT.pilihan as jawabannya_user, count(*) as jumlah, pertanyaan.pilihan as pertanyaan_pilihan
-from yfktn_surveykepuasan_jawaban as jawaban, 
-    yfktn_surveykepuasan_ as survey, 
-    yfktn_surveykepuasan_pertanyaan as pertanyaan,
-JSON_TABLE(
-    jawaban.jawaban,
-    '$[*]' columns(pilihan varchar(30) path '$')
-) as JT 
-where survey.id = jawaban.survey_id and survey.slug = ? and pertanyaan.survey_id = survey.id
-    and pertanyaan.cara_menjawab = 'checkbox'
-group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user
+    jawaban.jawaban as jawabannya_user, count(*) as jumlah, pertanyaan.pilihan as pertanyaan_pilihan
+from yfktn_surveykepuasan_ as survey
+inner join yfktn_surveykepuasan_pertanyaan as pertanyaan on survey.id = pertanyaan.survey_id 
+    and pertanyaan.cara_menjawab in ('checkbox')
+inner join yfktn_surveykepuasan_jawaban as jawaban on survey.id = jawaban.survey_id 
+    and pertanyaan.id = jawaban.pertanyaan_id
+where survey.slug = ?
+group by survey.nama, pertanyaannya_id, pertanyaan.pertanyaan, jawabannya_user, pertanyaan_pilihan
 SQL;
 
         $hasilJawaban = collect(DB::select($sqlSelectorRadio, [$this->property('slug')]));
         $hasilJawaban = $hasilJawaban->merge(
-            DB::select($sqlSelectorCheckBox, [$this->property('slug')])
+            $this->processForCheckboxGroupingCount(DB::select($sqlSelectorCheckBox, [$this->property('slug')]))
         );
         $untukGrafis = [];
         $indexWarna = [];
@@ -109,8 +106,60 @@ SQL;
 
     }
 
-    public function renderStrukturDataUntukTipeDataRadio()
+    /**
+     * Hasil dari query untuk tipe pertanyaan checkbox masih berbentuk JSON pada field jawabannya_user, 
+     * di mana membutuhkan proses lebih lanjut. Masalahnya adalah: fungsi JSON_TABLE ternyata tidak bisa
+     * diandalkan sementara karena server di production tidak menggunakan versi mysql yang dibutuhkan.
+     * Jadi di sini lakukan perhitungan ulang dari hasil query dengan cara merekontruksi ulang hasil query
+     * asli sehingga kita bisa mendapatkan nilai yang telah dikategorikan berdasarkan jawaban dengan
+     * tipe checkbox (bisa memilih lebih dari satu).
+     * @return array 
+     */
+    public function processForCheckboxGroupingCount(array $originalResult)
     {
-        
+        $groupingQuestion = [];
+        $groupingCounter = [];
+        foreach($originalResult as $theRow){
+            $theIdKey = $theRow->pertanyaannya_id;
+            // ----- GROUPING Question
+            if(!isset($groupingQuestion[$theIdKey])) {
+                $buffer = json_decode(json_encode($theRow), true);
+                $groupingQuestion[$theIdKey] = array_filter($buffer, 
+                    function($key) {
+                        if($key == 'jawabannya_user' || $key == 'jumlah') {
+                            return false;
+                        }
+                        return true;
+                    }, ARRAY_FILTER_USE_KEY);
+            }
+            // ----- GROUPING Counter!
+            // ambil dulu nilai pilihan jawabannya user! dan decode ke array untuk dibaca!
+            $arrayValue = json_decode($theRow->jawabannya_user);
+            // baru hitung ke dalam groupingCounter nya
+            foreach($arrayValue as $key => $value) {
+                if(!isset($groupingCounter[$theIdKey])) {
+                    $groupingCounter[$theIdKey] = []; // bikin array baru
+                }
+                if(isset($groupingCounter[$theIdKey][$value])) {
+                    $groupingCounter[$theIdKey][$value] += $theRow->jumlah;
+                } else {
+                    $groupingCounter[$theIdKey][$value] = $theRow->jumlah;
+                }
+            }
+        }
+        // di sini semestinya sudah semua tergroupkan maka susun ulang lagi 
+        $newData = [];
+        $counter = -1;
+        foreach($groupingCounter as $theIdKey => $checkboxGroupedValue) {
+            foreach($checkboxGroupedValue as $value => $count) {
+                $newData[++$counter] = $groupingQuestion[$theIdKey];
+                $newData[$counter]['jawabannya_user'] = $value;
+                $newData[$counter]['jumlah'] = $count;
+                // to make it sync with the other results then we need to convert each items
+                // from assoc array to object.
+                $newData[$counter] = (Object) $newData[$counter];
+            }
+        }
+        return $newData;
     }
 }
